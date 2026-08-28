a1c6fd0e8ea908552c91705e4c60c4511b463eb2
rousseauarthur2003-cloud <rousseauarthur2003@gmail.com>
2026-08-28 20:15:28 +0200

    Fix import statement formatting in __root.tsx

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -1,4 +1,4 @@
-Voici le code : import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import {
   Outlet,
   Link,
